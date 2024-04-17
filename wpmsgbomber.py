8e3a4543bfc8ce4017438e998105815454db0ea9
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-04-18 00:38:56 +0530

    update

---
+++ wpmsgbomber.py
@@ -0,0 +1,10 @@
+import pyautogui as pag
+import time
+
+time.sleep(1);
+for i in range (0,100):
+    pag.write("I Love You")
+
+    pag.press('enter')
+
+#after run this program click on a message box where you want to send the message 
