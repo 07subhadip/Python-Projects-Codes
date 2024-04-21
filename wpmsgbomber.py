81819ae1c4899e6fae30d1060f1db7aba5bc0906
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-04-21 23:26:11 +0530

    update

--- wpmsgbomber.py
+++ wpmsgbomber.py
@@ -1,10 +1,9 @@
 import pyautogui as pag
 import time
 
-time.sleep(5);
-for i in range (0,100):
-    pag.write("Enter msg here")
-
+time.sleep(5)
+for i in range (0,1001):
+    pag.write("Enter your text")
     pag.press('enter')
 
 #after run this program click on a message box where you want to send the message 
