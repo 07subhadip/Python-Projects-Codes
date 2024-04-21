a855ac3f49535da16e682144b3f05fb4059d8755
07subhadip <91666506+07subhadip@users.noreply.github.com>
2024-04-21 16:21:55 +0530

    update

--- wpmsgbomber.py
+++ wpmsgbomber.py
@@ -1,10 +1,12 @@
 import pyautogui as pag
 import time
 
-time.sleep(1);
+time.sleep(5);
 for i in range (0,100):
-    pag.write("I Love You")
+    pag.write("Enter msg here")
 
     pag.press('enter')
 
 #after run this program click on a message box where you want to send the message 
+
+# +91 80162 34988
